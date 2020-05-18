87b6d3415d6c79752d1a74d4e9f90efbf4721899
Jan Altensen <info@stricted.net>
2020-05-18 12:28:10 +0200

    universal7580-common: generate vulkan and opencl symlinks in device tree
    Change-Id: I25501e0ffcb86b8e21ee44c648b9f14bbb931b8b

--- Android.mk
+++ Android.mk
@@ -19,5 +19,17 @@ LOCAL_PATH := device/samsung/universal7580-common
 ifneq ($(filter a3xelte a5xelte a7xelte gvwifi gvlte j7elte j7xlte s5neolte, $(TARGET_DEVICE)),)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
+include $(CLEAR_VARS)
+
+EGL_LIBS := libGLES_mali.so libOpenCL.so libOpenCL.so.1 libOpenCL.so.1.1 hw/vulkan.exynos5.so
+
+EGL_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/lib/,$(EGL_LIBS))
+$(EGL_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "EGL 32 lib link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /vendor/lib/egl/libGLES_mali.so $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(EGL_SYMLINKS)
 
 endif
